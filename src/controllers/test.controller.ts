8da30d2e203f968782424825847dddfbb421008d
Tharindu Lashan <tharindufernandoofficial@gmail.com>
2026-04-25 01:48:14 +0530

    env test

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -1,13 +1,14 @@
-import { Request, Response } from 'express';
-import { pool } from '../config/db';
+import { Request, Response } from "express";
+import { pool } from "../config/db";
+require("dotenv").config();
 
 export const testDB = async (req: Request, res: Response) => {
   try {
-    const result = await pool.query('SELECT NOW()');
+    const result = await pool.query("SELECT NOW()");
 
     res.json({
       success: true,
-      message: 'Database connected successfully ✅',
+      message: "Database connected successfully ✅",
       time: result.rows[0].now,
     });
   } catch (error: any) {
@@ -15,7 +16,7 @@ export const testDB = async (req: Request, res: Response) => {
 
     res.status(500).json({
       success: false,
-      message: 'Database connection failed ❌',
+      message: "Database connection failed ❌",
       error: error.message,
     });
   }
@@ -23,18 +24,18 @@ export const testDB = async (req: Request, res: Response) => {
 
 export const testApi = async (req: Request, res: Response) => {
   try {
-
     res.json({
       success: true,
-      message: 'Server connected successfully ✅',
+      message: "Server connected successfully ✅",
       time: new Date(),
+      dbhost: process.env.PG_HOST,
     });
   } catch (error: any) {
     console.error(error);
 
     res.status(500).json({
       success: false,
-      message: 'Server connection failed',
+      message: "Server connection failed",
       error: error.message,
     });
   }
